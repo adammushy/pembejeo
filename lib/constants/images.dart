3e3da3cd796a3a2af38087ab2b5641916ac59eb3
adammushy <adamprosper99@gmail.com>
2024-05-25 01:17:37 +0300

    Update images.dart

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -3,9 +3,8 @@ class Images {
   static const String loveIcon = 'assets/icons/love.png';
 
   // for Image
-  static const String splashImage = 'assets/images/love.png';
+  static const String splashImage = 'assets/images/pembejeo2.jpg';
   static const String onboard_1 = 'assets/images/onb1.jpg';
   static const String onboard_2 = 'assets/images/onb1.jpg';
   static const String onboard_3 = 'assets/images/onb1.jpg';
-
 }
